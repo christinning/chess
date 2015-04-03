6d3ad673c0cf3cfb803b6f5e6c1bd182402f64e9
Chris Tinning <chris.tinning@gmail.com>
2015-04-03 22:38:47 +0100

    Introduce asyn channels to handle mouse clicks

--- src/cljs/chess/core.cljs
+++ src/cljs/chess/core.cljs
@@ -1,7 +1,9 @@
 (ns chess.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [chess.board :as board]))
+            [chess.board :as board]
+            [cljs.core.async :refer [chan <! put!]]))
 
 
 
@@ -18,31 +20,39 @@
     (render [_]
       (dom/div #js {:className (piece-class-name p)} nil))))
 
-(defn square [[id p selected] owner {:keys [on-click]}]
+(defn square [[id p selected] owner {:keys [click-chan]}]
   (reify
     om/IRender
     (render [_]
       (dom/div
-       #js {:className (str "square" (if selected " selected")) :id id}
+       #js {:className (str "square" (if selected " selected"))
+            :id id
+            :onClick #(put! click-chan id)}
        (if p
          (om/build piece p))))))
 
-(defn main []
-  (om/root
-    (fn [{:keys [board selected]} owner]
-      (reify
-        om/IRender
-        (render [_]
-          (apply dom/div #js {:className "board"}
-                 (map
+(let [click-chan (chan)]
+  (defn main []
+    (om/root
+     (fn [{:keys [board selected] :as app-state} owner]
+       (reify
+         om/IRender
+         (render [_]
+           (apply dom/div #js {:className "board"}
+                  (map
                    (fn
                      [r]
                      (apply dom/div #js {:className "row"}
-                           (om/build-all square r)))
+                            (om/build-all square r {:opts {:click-chan click-chan}})))
                    (->> board
                         board/squares-and-pieces
                         (map (fn [[s p]] [s p (= s selected)]))
-                        board/in-rows))))))
-    app-state
-    {:target (. js/document (getElementById "app"))}))
+                        board/in-rows))))
+         om/IDidMount
+         (did-mount [_]
+           (go (while true
+                 (let [new-selection (<! click-chan)]
+                   (om/transact! app-state :selected (fn [_] new-selection))))))))
+     app-state
+     {:target (. js/document (getElementById "app"))})))
 
