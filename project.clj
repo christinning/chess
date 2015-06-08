4365e5dbea3987bc558d845ce03e822ecf7c74a3
Chris Tinning <chris.tinning@gmail.com>
2015-06-08 18:10:46 +0100

    swiched project file over to mirror that of the figwheel sample project

--- project.clj
+++ project.clj
@@ -1,64 +1,71 @@
-(defproject chess "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject fchess "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/clj" "src/cljs"]
+            :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                           [org.clojure/clojurescript "0.0-3211"]
+                           [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                           [ring "1.3.1"]
+                           [compojure "1.3.4"]
+                           [enlive "1.1.5"]
+                           [om "0.7.3"]
+                           [figwheel "0.3.3"]
+                           [environ "1.0.0"]]
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/clojurescript "0.0-3165"]
-                 [ring "1.3.1"]
-                 [compojure "1.3.4"]
-                 [enlive "1.1.5"]
-                 [om "0.7.3"]
-                 [figwheel "0.3.3"]
-                 [environ "1.0.0"]
-                 [com.cemerick/piggieback "0.2.1"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [weasel "0.7.0-SNAPSHOT"]
-                 [leiningen "2.5.0"]]
+            :plugins [[lein-cljsbuild "1.0.5"]
+                      [lein-figwheel "0.3.3"]
+                      [lein-environ "1.0.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-environ "1.0.0"]]
+            :source-paths ["src" "src/cljs"]
 
-  :min-lein-version "2.5.0"
+            :clean-targets ^{:protect false} ["resources/public/js/" "target"]
 
-  :uberjar-name "chess.jar"
+            :cljsbuild {
+                        :builds [{:id "dev"
+                                  :source-paths ["src/cljs"]
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
-                             :compiler {:output-to     "resources/public/js/app.js"
-                                        :output-dir    "resources/public/js/out"
-                                        :source-map    "resources/public/js/out.js.map"
-                                        :preamble      ["react/react.min.js"]
-                                        :externs       ["react/externs/react.js"]
-                                        :optimizations :none
-                                        :pretty-print  true}}}}
+                                  :figwheel { :on-jsload "chess.core/on-js-reload" }
 
-  :profiles {:dev {:source-paths ["env/dev/clj"]
-                   :dependencies [[com.cemerick/piggieback "0.2.1"]
-                                  [org.clojure/tools.nrepl "0.2.10"]
-                                  [figwheel-sidecar "0.3.3"]]
+                                  :compiler {:main chess.core
+                                             :asset-path "js/"
+                                             :output-to "resources/public/js/app.js"
+                                             :output-dir "resources/public/js/"
+                                             :source-map-timestamp true }}
+                                 {:id "min"
+                                  :source-paths ["src/cljs"]
+                                  :compiler {:output-to "resources/public/js/app.js"
+                                             :main chess.core
+                                             :optimizations :advanced
+                                             :pretty-print false}}]}
 
-                   :repl-options {:init-ns chess.server
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+            :figwheel {
+                       :http-server-root "" ;; default and assumes "resources"
+                       :server-port 10555 ;; default
+                       :css-dirs ["resources/public/css"] ;; watch and update CSS
 
-                   :plugins [[lein-figwheel "0.3.3"]]
+                       ;; Start an nREPL server into the running figwheel process
+                       :nrepl-port 7889
 
-                   :figwheel {:http-server-root "public"
-                              :port 3449
-                              :css-dirs ["resources/public/css"]}
+                       ;; Server Ring Handler (optional)
+                       ;; if you want to embed a ring handler into the figwheel http-kit
+                       ;; server, this is for simple ring servers, if this
+                       ;; doesn't work for you just run your own server :)
+                       ;; :ring-handler hello_world.server/handler
 
-                   :env {:is-dev true}
+                       ;; To be able to open files in your editor from the heads up display
+                       ;; you will need to put a script on your path.
+                       ;; that script will have to take a file path and a line number
+                       ;; ie. in  ~/bin/myfile-opener
+                       ;; #! /bin/sh
+                       ;; emacsclient -n +$2 $1
+                       ;;
+                       ;; :open-file-command "myfile-opener"
 
-                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
+                       ;; if you want to disable the REPL
+                       ;; :repl false
 
-             :uberjar {:hooks [leiningen.cljsbuild]
-                       :env {:production true}
-                       :omit-source true
-                       :aot :all
-                       :cljsbuild {:builds {:app
-                                            {:source-paths ["env/prod/cljs"]
-                                             :compiler
-                                             {:optimizations :advanced
-                                              :pretty-print false}}}}}})
+                       ;; to configure a different figwheel logfile path
+                       ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+                       })
