4fc90672dd19863cf2c95cb8b97d138137248770
Chris Tinning <chris.tinning@gmail.com>
2015-06-22 22:03:38 +0100

    Adds async test for clicking on squares to move pieces

--- test/cljs/chess/components_test.cljs
+++ test/cljs/chess/components_test.cljs
@@ -1,7 +1,8 @@
 (ns cljs.chess.components-test
   (:require-macros [cemerick.cljs.test
-                   :refer [is deftest with-test run-tests testing test-var]]
-                   [dommy.core :refer [sel1]])
+                   :refer [is deftest with-test run-tests testing test-var done]]
+                   [dommy.core :refer [sel1]]
+                   [cljs.core.async.macros :refer [go]])
   (:require [cemerick.cljs.test :as t]
             [dommy.core :as dommy]
             [chess.components :as components]
@@ -23,6 +24,12 @@
         append-node)
     (. js/document getElementById id)))
 
+(defn click-on! [root selectors]
+  (let [elem (sel1 root selectors)]
+    (let [e (.createEvent js/document "MouseEvents")]
+      (.initMouseEvent e "click" true true js/window 1 0 0)
+      (.dispatchEvent elem e))))
+
 (deftest test-container
          (let [c (container! "container-1")]
            (is (sel1 :#container-1))))
@@ -30,12 +37,23 @@
 (deftest test-square
          (let [c (container!)]
            (om/root components/square ["a4" :r true] {:target c})
-           (is (sel1 c ["#a4.square.selected" ".piece.black.rook" ]))))
+           (is (sel1 c ["#a4.square.selected" ".piece.black.rook"]))))
 
 (deftest test-board
          (let [c (container!)]
            (om/root components/board {:board start-position} {:target c})
            (is (sel1 c [:#a1 ".piece.white.rook"]))))
+
+(deftest ^:async test-board-interaction
+         (let [c (container!)]
+           (om/root components/board {:board start-position} {:target c})
+           (click-on! c "#d2")
+           (click-on! c "#d4")
+           (js/setTimeout
+             (fn []
+               (is (sel1 c ["#d4" ".piece.white.pawn"]))
+               (done))
+             500)))
 
 
 
