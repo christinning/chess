a976b39816b008f67888d681c38da2f38b76e6ac
Chris Tinning <chris.tinning@gmail.com>
2015-06-28 19:20:16 +0100

    Switch to putting typed message on the channel that handles clicks, we can now handle other events

--- src/cljs/chess/components.cljs
+++ src/cljs/chess/components.cljs
@@ -19,14 +19,14 @@
     (display-name [_]
       "piece")))
 
-(defn square [[id p selected] owner {:keys [click-chan]}]
+(defn square [[id p selected] owner {:keys [action-chan]}]
   (reify
     om/IRender
     (render [_]
       (dom/div
        #js {:className (str "square" (if selected " selected"))
             :id        id
-            :onClick   #(put! click-chan id)}
+            :onClick   #(put! action-chan {:type :click :square id})}
        (if p
          (om/build piece p))))
     om/IDisplayName
@@ -37,34 +37,38 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:click-chan (chan)})
+      {:action-chan (chan)})
     om/IRenderState
-    (render-state [this {click-chan :click-chan selected :selected}]
+    (render-state [this {action-chan :action-chan selected :selected}]
       (apply dom/div #js {:className "board"}
              (map
                (fn
                  [r]
                  (apply dom/div #js {:className "row"}
-                        (om/build-all square r {:opts {:click-chan click-chan}})))
+                        (om/build-all square r {:opts {:action-chan action-chan}})))
                (->> game
                     squares-and-pieces
                     (map (fn [[s p]] [s p (= s selected)]))
                     as-rows))))
     om/IDidMount
     (did-mount [_]
-      (let [click-chan (om/get-state owner :click-chan)]
+      (let [action-chan (om/get-state owner :action-chan)]
         (go-loop []
-                 (let [new-selection (<! click-chan)
-                       old-selection (om/get-state owner :selected)]
-                   (if old-selection
-                     (do
-                       (om/transact! app-state
-                                     (fn [{:keys [game hist]}]
-                                       {:game (move game old-selection new-selection)
-                                        :hist (vec (conj (seq hist) game))}))
-                       (om/set-state! owner :selected nil))
-                     (if (in (deref game) new-selection)
-                       (om/set-state! owner :selected new-selection))))
+                 (let [message (<! action-chan)
+                       message-type (message :type)]
+                   (println message-type)
+                   (condp = message-type
+                     :click (let [new-selection (message :square)
+                                  old-selection (om/get-state owner :selected)]
+                              (if old-selection
+                                (do
+                                  (om/transact! app-state
+                                                (fn [{:keys [game hist]}]
+                                                  {:game (move game old-selection new-selection)
+                                                   :hist (vec (conj (seq hist) game))}))
+                                  (om/set-state! owner :selected nil))
+                                (if (in (deref game) new-selection)
+                                  (om/set-state! owner :selected new-selection))))))
                  (recur))))
     om/IDisplayName
     (display-name [_]
